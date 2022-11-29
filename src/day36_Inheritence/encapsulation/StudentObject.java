64d7768fa5ff09ba34bb2db0c56f2656e26d94ca
Ekrem Meşe <ekremmese1@gmail.com>
2022-11-29 18:56:06 +0300

    New code fragments added.

---
+++ src/day36_Inheritence/encapsulation/StudentObject.java
@@ -0,0 +1,18 @@
+package day36_Inheritence.encapsulation;
+
+public class StudentObject {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Ali", 2, 'M', 'A', "Cydeo School");
+
+        student1.setAge(39);
+        System.out.println(student1);
+
+    }
+
+}
+
+
+//1:13:01
+
