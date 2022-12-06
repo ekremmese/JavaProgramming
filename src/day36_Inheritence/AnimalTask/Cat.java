733f138b029e691ca739c2de86a4ec2f0cdc24f1
Ekrem Meşe <ekremmese1@gmail.com>
2022-12-06 18:23:40 +0300

    New code fragments added.

---
+++ src/day36_Inheritence/AnimalTask/Cat.java
@@ -0,0 +1,13 @@
+package day36_Inheritence.AnimalTask;
+
+public class Cat extends Animal{
+
+    public void meow(){
+        System.out.println(name + " is meowing");
+    }
+
+    public void scratch(){
+        System.out.println(name + " is scratching");
+    }
+
+}
