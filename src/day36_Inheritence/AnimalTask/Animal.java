811d914f2b5b8f95584071fabb2b350cf6292e55
Ekrem Meşe <ekremmese1@gmail.com>
2022-12-06 18:20:03 +0300

    New code fragments added.

--- src/day36_Inheritence/AnimalTask/Animal.java
+++ src/day36_Inheritence/AnimalTask/Animal.java
@@ -42,5 +42,6 @@ public class Animal {
                 ", color='" + color + '\'' +
                 '}';
     }
+
 }
 
---
+++ src/day36_Inheritence/AnimalTask/Dog.java
@@ -0,0 +1,14 @@
+package day36_Inheritence.AnimalTask;
+
+public class Dog extends Animal {
+
+    public void bark(){
+        System.out.println(name + " is barking.");
+    }
+
+
+}
+
+
+//2:07:09
+
