98116bb83ee4e8b887ce50f56f92c50f3a734ae4
Ekrem Meşe <ekremmese1@gmail.com>
2022-12-06 18:26:00 +0300

    New code fragments added.

---
+++ src/day36_Inheritence/AnimalTask/Tiger.java
@@ -0,0 +1,13 @@
+package day36_Inheritence.AnimalTask;
+
+public class Tiger extends Animal{
+
+    public void hunt(){
+        System.out.println(name + " is huntinh");
+    }
+
+    public void roar(){
+        System.out.println(name + " is roaring");
+    }
+
+}
