43db0760b54539c7aa981470d654a77936a2d382
Ekrem Meşe <ekremmese1@gmail.com>
2022-12-16 13:17:24 +0300

    New code fragments added.

---
+++ src/day38_Inheritance/carTask/BMW.java
@@ -0,0 +1,10 @@
+package day38_Inheritance.carTask;
+
+public class BMW extends Car{
+
+    public BMW( String model, int year, double price, String color, double miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+
+}
