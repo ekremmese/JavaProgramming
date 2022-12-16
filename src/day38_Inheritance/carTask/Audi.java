3047d9482a7289b6e35fa422755df2db686beb3c
Ekrem Meşe <ekremmese1@gmail.com>
2022-12-16 12:42:02 +0300

    New code fragments added.

---
+++ src/day38_Inheritance/carTask/Audi.java
@@ -0,0 +1,12 @@
+package day38_Inheritance.carTask;
+
+public class Audi extends Car{
+
+    public Audi(String model, int year, double price, String color, double miles) {
+        super("Audi", model, year, price, color, miles);
+    }
+
+
+
+
+}
